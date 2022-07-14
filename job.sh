d963b9018c9f4d8d128188c2977aedac0572e185
mattsong <407735995@qq.com>
2022-07-14 21:00:56 +0800

    add multi-site training module

--- job.sh
+++ job.sh
@@ -4,6 +4,7 @@ batch_size=1
 patch_size=80
 iterations=300
 epochs=300
+num_sites=5
 
 datapath=data
 savepath=output
@@ -15,6 +16,8 @@ export PYTHONPATH="/content/PyTorch-RFNet:${PYTHONPATH}"
 #train
 python3 rfnet/train.py --batch_size $batch_size --patch_size $patch_size --iter_per_epoch $iterations --datapath $datapath --savepath $savepath --num_epochs $epochs --lr 2e-4 --region_fusion_start_epoch 20 
 
+# multisite_train
+#python3 rfnet/multisite_train.py --num_sites $num_sites --batch_size $batch_size --patch_size $patch_size --iter_per_epoch $iterations --datapath $datapath --savepath $savepath --num_epochs $epochs --lr 2e-4 --region_fusion_start_epoch 20 
+
 #test
 python3 rfnet/test.py --batch_size $batch_size --patch_size $patch_size --datapath $datapath --savepath $savepath --checkpoint $checkpoint
-
